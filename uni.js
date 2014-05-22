ae2bd5a3b6e29ac0421a00b6a7142690ae630eb2
James Wallace <sausages@gmail.com>
2014-05-21 23:13:43 -0400

    Made the hashes/anchor links work properly with the accordion feature.

--- uni.js
+++ uni.js
@@ -1,3 +1,11 @@
+$.fn.scrollView = function () {
+	return this.each(function () {
+		$('html, body').animate({
+			scrollTop: $(this).offset().top
+		}, 1000);
+	});
+}
+
 function server(data) {
 	$.ajax({
 		url: "ajax.php",
@@ -57,9 +65,32 @@ $(document).ready(function() {
 		heightStyle: "content"
 	});
 
-	var myVideoPlayer = document.getElementById('video');
-
-	myVideoPlayer.addEventListener('loadedmetadata', function(e) {
-		//console.log(e);
+	$(".modules").accordion({
+		active: false,
+		collapsible: true,
+		header: "h3",
+		heightStyle: "content"
 	});
+
+	if (window.location.hash) {
+		var hash = window.location.hash;
+		var type = hash.substring(1, 2);
+
+		// find the element that has this 
+		var el = null;
+
+		if (type == "c") {
+			el = $("body").find(hash).closest('h2');
+			el.trigger('click').scrollView();
+		} else if (type == "m") {
+			// we have to hit the parent first
+			el = $("body").find(hash).closest('h3');
+
+			var p = el.parents();
+			$(p[2].childNodes[0]).trigger('click');
+
+			el.trigger('click');
+			el.scrollView();
+		}
+	}
 });
